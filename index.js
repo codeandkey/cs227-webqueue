0df30d376c275a6ab423489988fcf981de1d0726
Justin Stanley <jtst@iastate.edu>
2021-02-03 18:40:36 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http)
+
+const AD_KEY = process.env.WEBQUEUE_ADMIN_KEY;
+const PORT = process.env.PORT || 8124;
+
+let queues = new Array(20); // queue state
+
+for (var i = 0; i < queues.length; ++i) {
+    queues[i] = [];
+}
+
+if (!AD_KEY) {
+    console.error("Please set WEBQUEUE_ADMIN_KEY before launching.");
+    process.exit(1);
+}
+
+app.set('view engine', 'pug');
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+    console.log(`Serving index to ${res.socket.remoteAddress}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('Accepted socketio connection');
+
+    socket.on('add', (sect, name) => {
+        if (!Number.isInteger(sect) || sect < 0 || sect > queues.length) {
+            return socket.emit('q_error', 'Invalid section');
+        }
+
+        for (var i = 0; i < queues[sect].length; ++i) {
+            if (queues[sect][i] == name) {
+                return socket.emit('q_error', 'Already queued!');
+            }
+        }
+
+        queues[sect].push(name);
+
+        io.emit('queues', queues);
+
+        return socket.emit('message', 'Queued!');
+    });
+
+    socket.on('remove', (sect, ind, key) => {
+        if (key != AD_KEY) {
+            return socket.emit('q_error', 'Invalid control key');
+        }
+
+        if (!Number.isInteger(sect) || sect < 0 || sect > queues.length) {
+            return socket.emit('q_error', 'Invalid section');
+        }
+
+        if (ind < 0 || ind >= queues[sect].length) {
+            return socket.emit('q_error', 'Invalid index');
+        }
+
+        var n = queues[sect][ind];
+
+        queues[sect].splice(ind, 1);
+
+        io.emit('queues', queues);
+
+        return socket.emit('message', `Removed ${n} from queue.`);
+    });
+
+    socket.on('request', () => {
+        // Broadcast update
+        io.emit('queues', queues);
+    });
+})
+
+http.listen(PORT, () => {
+    console.log(`Listening on ${PORT}, ready for requests`);
+});
---
+++ static/js/main.js
@@ -0,0 +1,64 @@
+const socket = io();
+
+var queues = [];
+var err_timer;
+
+function reloadUi() {
+    // Get the current section selection
+    var cur_sect = parseInt($('#section-input').val());
+
+    $('#queue-entries').empty();
+
+    for (var i = 0; i < queues[cur_sect].length; ++i) {
+        var id = i;
+
+        console.log('adding element ' + id);
+        $('#queue-entries').append('<div class="row queue-entry h2 text-center text-light" id="queue-element' + id + '">' + (i + 1) + '. ' + queues[cur_sect][id] + '</div>');
+
+        $('#queue-element' + i).click(() => {
+            console.log('clicked on ' + id);
+            socket.emit('remove', cur_sect, id, $('#key-input').val());
+            console.log('sending remove: ', cur_sect, id, $('#key-input').val());
+        });
+    }
+}
+
+socket.on('queues', (states) => {
+    // Update queue states.
+    queues = states;
+
+    // Remove waiting msg.
+    $('#waiting-msg').css('display', 'none');
+
+    console.log('Received ' + states.length + ' queue states.');
+
+    // Refresh the current UI
+    reloadUi();
+});
+
+socket.on('q_error', (msg) => {
+    // Make error alert visible, set timer
+    err_timer = setTimeout(() => {
+        $('#error-msg').css('opacity', 0.0);
+        clearTimeout(err_timer);
+    }, 5000);
+
+    $('#error-msg').text(msg);
+    $('#error-msg').css('opacity', 1.0);
+});
+
+$(document).ready(() => {
+    $('#btn-queue').click(() => {
+        var cur_sect = parseInt($('#section-input').val());
+    
+        socket.emit('add', cur_sect, $('#name-input').val());
+    
+        console.log('Send add request for ' + cur_sect + ' : ' + $('#name-input').val());
+    });
+
+    $('#section-input').change(() => {
+        reloadUi();
+    });
+});
+
+socket.emit('request');
